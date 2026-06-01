e1d04b251faa77a74865f376f37d8c81c0ee9ff8
Kaspx3 <mr.kaspersobczyk@gmail.com>
2026-06-01 12:14:20 +0200

    1.1

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const images = ["media/happy.png", "media/angry.png", "media/sad.png"];
-let currentIndex = 0;
+//let currentIndex = 0;
+//
+//function changeImage() {
+//    currentIndex = (currentIndex + 1) % images.length;
+//    document.getElementById("happy").src = images[currentIndex];
+//}
 
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById("happy").src = images[currentIndex];
+function toHappy() {
+    document.getElementById("happy").src = images["media/happy.png"]
+}
+
+function toSad() {
+    document.getElementById("happy").src = images["media/sad.png"]
+}
+
+function toAngry() {
+    document.getElementById("happy").src = images["media/angry.png"]
 }
