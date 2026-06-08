fdc0680a2ce20823960da652e4cd8559a9688840
Kaspx3 <mr.kaspersobczyk@gmail.com>
2026-06-08 12:15:35 +0200

    hunger bar not finished tho!!

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function toHappy() {
     document.getElementById("happy").src = "media/happy.png"
+    document.getElementById("reacth3").textContent = "Kitty loves pets :3"
 }
 
 function toSad() {
     document.getElementById("happy").src = "media/sad.png"
+    document.getElementById("reacth3").textContent = "Kitty rebukes the sadness button, why does it even exist?"
 }
 
 function toAngry() {
     document.getElementById("happy").src = "media/angry.png"
+    document.getElementById("reacth3").textContent = "Don't you know cats hate water? you fool."
+}
+
+let value = 100;
+
+// drains the bar slowly
+setInterval(() => {
+  value -= 0.08; // speed of draining
+  if (value < 0) value = 0;
+  document.getElementById("bar").style.width = value + "%";
+}, 100);
+
+function resetBar() {
+  value = 100;
+  document.getElementById("bar").style.width = "100%";
 }
