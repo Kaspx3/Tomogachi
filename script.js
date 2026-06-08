df243c5a5521d54de69e1a939ce79af89ffdce46
Kaspx3 <mr.kaspersobczyk@gmail.com>
2026-06-08 11:20:09 +0200

    buttons add and fix

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-const images = ["media/happy.png", "media/angry.png", "media/sad.png"];
-//let currentIndex = 0;
-//
-//function changeImage() {
-//    currentIndex = (currentIndex + 1) % images.length;
-//    document.getElementById("happy").src = images[currentIndex];
-//}
-
 function toHappy() {
-    document.getElementById("happy").src = images["media/happy.png"]
+    document.getElementById("happy").src = "media/happy.png"
 }
 
 function toSad() {
-    document.getElementById("happy").src = images["media/sad.png"]
+    document.getElementById("happy").src = "media/sad.png"
 }
 
 function toAngry() {
-    document.getElementById("happy").src = images["media/angry.png"]
+    document.getElementById("happy").src = "media/angry.png"
 }
