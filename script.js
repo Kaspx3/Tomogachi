1245a03a63ef8a830204ae0f15d7c9526e4f3682
Kaspx3 <mr.kaspersobczyk@gmail.com>
2026-06-08 14:53:08 +0200

    finished release 1.0

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toHappy() {
 function toSad() {
     document.getElementById("happy").src = "media/sad.png"
 
-    document.getElementById("reacth3").textContent = "Kitty rebukes the sadness button, why does it even exist?"
+    document.getElementById("reacth3").textContent = "Kitty doesn't like homework.. It wants to meow and cuddle"
 
     document.body.classList.add("sad");
     document.body.classList.remove("happy");
@@ -26,16 +26,46 @@ function toAngry() {
     document.body.classList.remove("sad");
 }
 
-let value = 100;
+let hunger = 100;
 
-// drains the bar slowly
 setInterval(() => {
-  value -= 0.08; // speed of draining
-  if (value < 0) value = 0;
+  value -= 0.08;
+  if (hunger < 0) value = 0;
+  if (value <= 0) {
+    value = 0;
+    kittyIsHungry();
+}
   document.getElementById("bar").style.width = value + "%";
 }, 100);
 
 function resetBar() {
-  value = 100;
+  hunger = 100;
   document.getElementById("bar").style.width = "100%";
+}
+
+let value = 100;
+
+setInterval(() => {
+    value -= 0.1;
+
+    if (value < 0) value = 0;
+
+    document.getElementById("bar").style.width = value + "%";
+    document.getElementById("bar-text").textContent = "Hunger " + Math.round(value) + "%";
+}, 100);
+
+function resetBar() {
+    value = 100;
+    document.getElementById("bar").style.width = "100%";
+    document.getElementById("bar-text").textContent = "100%";
+}
+
+function kittyIsHungry() {
+    document.getElementById("happy").src = "media/sad.png";
+    document.getElementById("reacth3").textContent =
+        "Kitty is starving... feed it! And don't forget to pet it too as compensation for making it STARVE";
+    
+    document.body.classList.add("sad");
+    document.body.classList.remove("happy");
+    document.body.classList.remove("angry");
 }
