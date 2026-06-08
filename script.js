01f80c542fdd6ae537d9a73e50d83f2221e93676
KUIJEN <91477681+Kuijen@users.noreply.github.com>
2026-06-08 12:13:36 +0200

    Added color palette swap

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function toHappy() {
     document.getElementById("happy").src = "media/happy.png"
+    document.body.classList.add("happy");
+    document.body.classList.remove("sad");
+    document.body.classList.remove("angry");
 }
 
 function toSad() {
     document.getElementById("happy").src = "media/sad.png"
+    document.body.classList.add("sad");
+    document.body.classList.remove("happy");
+    document.body.classList.remove("angry");
 }
 
 function toAngry() {
     document.getElementById("happy").src = "media/angry.png"
+    document.body.classList.add("angry");
+    document.body.classList.remove("happy");
+    document.body.classList.remove("sad");
 }
